a7b382d242416568c5e4fd56e1b71a9fefe6edfa
Yujith Kodagoda <119272981+TheRealYujith@users.noreply.github.com>
2025-05-29 17:46:41 +0530

    Removed the fetching of corporate splits data

--- fetchingScripts/fetchCorporateEvents.py
+++ fetchingScripts/fetchCorporateEvents.py
@@ -4,7 +4,6 @@ import pandas as pd
 def fetch_corporate_events(ticker: str, start_date: str, end_date: str) -> dict:
     stock = yf.Ticker(ticker)
     dividends = stock.dividends.loc[start_date:end_date].reset_index()
-    splits = stock.splits.loc[start_date:end_date].reset_index()
     earnings = stock.income_stmt.T.reset_index() # Annual earnings
     
     # Handling calendar when its a dict or dataframe
@@ -18,7 +17,6 @@ def fetch_corporate_events(ticker: str, start_date: str, end_date: str) -> dict:
 
     return {
         "dividends": dividends,
-        "splits": splits,
         "earnings": earnings,
         "calendar": calendar
     }
@@ -27,6 +25,5 @@ def fetch_corporate_events(ticker: str, start_date: str, end_date: str) -> dict:
 if __name__ == "__main__":
     corporate_events_data = fetch_corporate_events("AAPL", "2023-12-31", "2025-01-01")
     corporate_events_data["dividends"].to_csv("corporate_dividends_AAPL.csv", index=False)
-    corporate_events_data["splits"].to_csv("corporate_splits_AAPL.csv", index=False)
     corporate_events_data["earnings"].to_csv("corporate_earnings_AAPL.csv", index=False)
     corporate_events_data["calendar"].to_csv("corporate_calendar_AAPL.csv", index=False)
