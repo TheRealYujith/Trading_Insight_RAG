015a38f35f4934a1fc801bb5d9b2f4af04365015
Yujith Kodagoda <119272981+TheRealYujith@users.noreply.github.com>
2025-05-30 00:03:43 +0530

    Removed unnecessary data fetching

--- fetchingScripts/fetchFundamentalData.py
+++ fetchingScripts/fetchFundamentalData.py
@@ -6,14 +6,12 @@ def fetch_fundamental_data(ticker: str) -> dict:
     stock = yf.Ticker(ticker)
     financials = stock.financials
     balance_sheet = stock.balance_sheet
-    income_statement = stock.income_stmt
     
     return {
         # .T is used to swap rows to columns and vice versa
         # .reset_index() is used so the dates are now a column rather than the index
         "financials": financials.T.reset_index(),  
-        "balance_sheet": balance_sheet.T.reset_index(),
-        "income_statement": income_statement.T.reset_index()
+        "balance_sheet": balance_sheet.T.reset_index()
     }
     
 # Testing block
@@ -21,4 +19,3 @@ if __name__ == "__main__":
     fundamental_data = fetch_fundamental_data("AAPL")
     fundamental_data["financials"].to_csv("fundamentals_financials_AAPL.csv", index=False)
     fundamental_data["balance_sheet"].to_csv("fundamentals_balance_sheet_AAPL.csv", index=False)
-    fundamental_data["income_statement"].to_csv("fundamentals_income_statement_AAPL.csv", index=False)
