de3aad03abb49daa45414d29f4b7788d671dd516
Yujith Kodagoda <119272981+TheRealYujith@users.noreply.github.com>
2025-06-01 15:38:27 +0530

    Changed the clean_metadata function, switched to a domain specific embedding model and created a new collection

--- preprocessing/embeddings.py
+++ preprocessing/embeddings.py
@@ -1,19 +1,31 @@
 from sentence_transformers import SentenceTransformer
 from typing import List, Dict
+from datetime import datetime
 import chromadb
-from chromadb.config import Settings
+import pandas as pd
+
+def clean_metadata(metadata: Dict) -> Dict:
+    cleaned = {}
+    for k, v in metadata.items():
+        if isinstance(v, (str, int, float, bool)) or v is None:
+            cleaned[k] = v
+        elif isinstance(v, (datetime, pd.Timestamp)):
+            cleaned[k] = v.strftime("%Y-%m-%d")  # Or include time: "%Y-%m-%d %H:%M:%S"
+        else:
+            cleaned[k] = str(v)
+    return cleaned
 
 def embedd_and_store_in_chroma(chunks: List[Dict]):
     # Initialize ChromaDB client (local)
-    chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="./chroma_store"))
-    collection = chroma_client.get_or_create_collection(name="apple_market_data")
+    chroma_client = chromadb.PersistentClient(path="./chroma_store")
+    collection = chroma_client.get_or_create_collection(name="apple_market_data_finbert")
 
-    texts = [chunk["text"] for chunk in chunks]
+    texts = [" ".join(chunk["text"]) if isinstance(chunk["text"], list) else str(chunk["text"])for chunk in chunks]
     ids = [chunk["id"] for chunk in chunks]
-    metadatas = [chunk.get("metadata", {}) for chunk in chunks]
+    metadatas = [clean_metadata(chunk.get("metadata", {})) for chunk in chunks]
     
     # Load model and create embeddings
-    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    model = SentenceTransformer("yiyanghkust/finbert-tone")
     embeddings = model.encode(texts).tolist()
 
     # Add to ChromaDB
